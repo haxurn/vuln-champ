fbb7219a87803d47cbfe5f78d3f8ba1fcc56192e
Haxurn <samitesfaye726@gmail.com>
2024-12-24 16:41:42 +0300

    feat: add utility for file uploads

---
+++ backend/utils/upload.util.ts
@@ -0,0 +1,31 @@
+// backend/utils/upload.ts
+
+import multer from 'multer';
+import path from 'path';
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const filename = `badge-${Date.now()}${ext}`;
+    cb(null, filename);
+  },
+});
+
+const fileFilter = (req: any, file: any, cb: any) => {
+  const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error('Invalid file type. Only JPEG, PNG, and GIF are allowed.'));
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+});
+
+export default upload;
