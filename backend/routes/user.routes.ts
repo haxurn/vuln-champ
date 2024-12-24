08fb6168aff7d6bc2c584c42f04fbec74b864088
Haxurn <samitesfaye726@gmail.com>
2024-12-24 17:28:23 +0300

    feat: modify user routes to handle avatar upload during user creation and update

--- backend/routes/user.routes.ts
+++ backend/routes/user.routes.ts
@@ -1,18 +1,19 @@
-// backend/routes/user.routes.ts
 import { Router } from 'express';
 import userController from '../controllers/user.controller'; 
 import { validateToken } from '../middleware/auth.middleware'; 
 import { checkAdminRole } from '../middleware/role.middleware'; 
+import upload from '../utils/upload.util';  
 
 const router = Router();
 
-router.post('/create', userController.createUser);
+router.post('/create', upload.single('avatar'), userController.createUser);
 
 router.get('/get/:id', validateToken, userController.getUserById);
 
-router.get('/all', validateToken, validateToken, userController.getAllUsers);
+router.get('/all', validateToken, userController.getAllUsers);
 
-router.patch('/update/:id', validateToken, userController.updateUserById);
+router.patch('/update/:id', upload.single('avatar'), validateToken, userController.updateUserById);
+
 
 router.delete('/delete/:id', validateToken, checkAdminRole, userController.deleteUserById);
 
