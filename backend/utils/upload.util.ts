444ae8e663b30111e9091df5fbb994dbff347d06
Haxurn <samitesfaye726@gmail.com>
2024-12-24 17:28:58 +0300

    chore: enhance upload utility to ensure upload directories exist for avatars

--- backend/utils/upload.util.ts
+++ backend/utils/upload.util.ts
@@ -1,15 +1,31 @@
-// backend/utils/upload.ts
-
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
+
+const ensureUploadDirsExist = () => {
+  const avatarFolder = 'uploads/avatars';
+  const uploadsFolder = 'uploads';
+
+  if (!fs.existsSync(uploadsFolder)) {
+    fs.mkdirSync(uploadsFolder, { recursive: true });
+  }
+
+  if (!fs.existsSync(avatarFolder)) {
+    fs.mkdirSync(avatarFolder, { recursive: true });
+  }
+};
+
+ensureUploadDirsExist();
+
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    const folder = file.fieldname === 'avatar' ? 'uploads/avatars' : 'uploads/';
+    cb(null, folder);
   },
   filename: (req, file, cb) => {
     const ext = path.extname(file.originalname);
-    const filename = `badge-${Date.now()}${ext}`;
+    const filename = `${file.fieldname}-${Date.now()}${ext}`;
     cb(null, filename);
   },
 });
