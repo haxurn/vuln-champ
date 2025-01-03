b62ae407570d629d02217a98031d713c1383613e
Haxurn <samitesfaye726@gmail.com>
2025-01-03 15:11:56 +0300

    feat: create utility function to get user session data

---
+++ util/getUserData.ts
@@ -0,0 +1,16 @@
+// util/getUserData.ts
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+
+const getUserSession = async () => {
+    const session = await auth.api.getSession(
+        {
+            headers: await headers(),
+        }
+    );
+
+    return session;
+};
+
+export default getUserSession;
