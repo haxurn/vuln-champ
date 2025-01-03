af9b604e1b68896ead8defab22b830b51a0cb280
Haxurn <samitesfaye726@gmail.com>
2025-01-04 01:43:36 +0300

    chore(config): update configuration files for project setup
    - Updated `tailwind.config.ts` to extend custom theme with new color and border settings.
    - Adjusted `tsconfig.json` for better compatibility and stricter type checking.
    - Enabled class-based dark mode in Tailwind CSS configuration.
    - Improved Prisma schema and database relations for User, Session, and Vulnerabilities models.
    - Enhanced TypeScript configuration for stricter type enforcement and module resolution.
    - Tweaked build setup and improved paths for better project structure and performance.

---
+++ hooks/query-provider.tsx
@@ -0,0 +1,13 @@
+// hooks/query-provider.tsx
+"use client";
+
+import { queryClient } from "@/util/query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+
+const QueryProviderComponent = ({ children }: { children: ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+export default QueryProviderComponent;
