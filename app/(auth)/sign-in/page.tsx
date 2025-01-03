b76e0205d871f214c385aebcd4120113d2ba8c75
Haxurn <samitesfaye726@gmail.com>
2025-01-04 01:22:40 +0300

    feat(auth): implement authentication system and session management
    - Created auth middleware to protect routes and manage user sessions.
    - Integrated better-fetch and better-auth for session handling.
    - Added session validation and redirection to landing page if not authenticated.
    - Set up Next.js server-side authentication flow for secure access control.
    - Defined session and user models for user authentication and session management.

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,188 @@
+// app/(auth)/sign-in/page.tsx
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { authClient } from "@/lib/auth-client";
+import { motion } from "framer-motion";
+import {
+  EyeIcon as EyeClosedIcon,
+  EyeIcon,
+  Github,
+  Loader2,
+} from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { BackgroundGradient } from "@/components/ui/background-gradient";
+import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
+
+export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [show, setShow] = useState(false);
+
+  const handleSignIn = async () => {
+    setLoading(true);
+    try {
+      const { data, error } = await authClient.signIn.email({
+        email: email,
+        password: password,
+        callbackURL: "/",
+      });
+
+      if (data) {
+        toast.success("Signed in successfully!");
+      }
+
+      if (error) {
+        toast.error("Signed in failed!");
+      }
+    } catch (error) {
+      console.error("Sign in failed:", error);
+      toast.error("Sign in failed. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleGithubSignIn = async () => {
+    setLoading(true);
+    try {
+      await authClient.signIn.social({
+        provider: "github",
+        callbackURL: "/",
+      });
+      toast.success("Signed in with GitHub successfully!");
+    } catch (error) {
+      console.error("GitHub sign in failed:", error);
+      toast.error("GitHub sign in failed. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <BackgroundBeamsWithCollision className="flex justify-center items-center min-h-screen w-full ">
+      <Toaster position="top-center" reverseOrder={false} />
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        {/* BackgroundGradient applied here with additional padding and rounded corners */}
+        <BackgroundGradient className="rounded-[22px] p-5 sm:p-10 bg-white dark:bg-zinc-900">
+          <Card className="w-[95%] mx-auto md:w-[22vw] shadow-sm md:shadow-lg rounded-lg  dark:bg-zinc-900">
+            <CardHeader className="space-y-1">
+              <CardTitle className="md:text-2xl text-xl font-bold text-center">
+                Sign In
+              </CardTitle>
+              <CardDescription className="text-center">
+                Enter your email below to login to your account
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-4">
+              <div className="space-y-2">
+                <Label htmlFor="email" className="text-sm font-medium">
+                  Email
+                </Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="joe@xample.com"
+                  required
+                  className="w-full text-sm placeholder:text-sm"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="password" className="text-sm font-medium">
+                  Password
+                </Label>
+                <div className="relative">
+                  <Input
+                    id="password"
+                    type={show ? "text" : "password"}
+                    placeholder="Enter your password"
+                    className="w-full text-sm placeholder:text-sm"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <Button
+                    type="button"
+                    variant="ghost"
+                    size="sm"
+                    className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+                    onClick={() => setShow(!show)}
+                  >
+                    {show ? (
+                      <EyeClosedIcon className="h-4 w-4 text-gray-500" />
+                    ) : (
+                      <EyeIcon className="h-4 w-4 text-gray-500" />
+                    )}
+                  </Button>
+                </div>
+              </div>
+              <Button
+                type="submit"
+                className="w-full  dark:bg-zinc-500"
+                disabled={loading}
+                onClick={handleSignIn}
+              >
+                {loading ? (
+                  <Loader2 size={16} className="animate-spin mr-2" />
+                ) : null}
+                {loading ? "Signing In..." : "Sign In"}
+              </Button>
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <span className="w-full border-t" />
+                </div>
+                <div className="relative flex justify-center text-xs uppercase">
+                  <span className="bg-background px-2 text-muted-foreground  dark:bg-zinc-900">
+                    Or continue with
+                  </span>
+                </div>
+              </div>
+              <Button
+                variant="outline"
+                className="w-full dark:bg-zinc-500"
+                onClick={handleGithubSignIn}
+                disabled={loading}
+              >
+                {loading ? (
+                  <Loader2 size={16} className="animate-spin mr-2" />
+                ) : (
+                  <Github className="mr-2 h-4 w-4" />
+                )}
+                GitHub
+              </Button>
+            </CardContent>
+            <CardFooter>
+              <p className="text-center text-sm text-muted-foreground w-full">
+                Don&apos;t have an account?{" "}
+                <Link
+                  href="/sign-up"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500 hover:underline"
+                >
+                  Sign up
+                </Link>
+              </p>
+            </CardFooter>
+          </Card>
+        </BackgroundGradient>
+      </motion.div>
+    </BackgroundBeamsWithCollision>
+  );
+}
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,4 +1,266 @@
-// frontend/app/(auth)/sign-up/page.tsx
+// app/(auth)/sing-up/page.tsx
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { authClient } from "@/lib/auth-client";
+import { motion } from "framer-motion";
+import { EyeIcon, EyeOffIcon, Loader2, Upload, X } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { BackgroundGradient } from "@/components/ui/background-gradient";
+import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
 
+
+export default function SignUpPage() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+  });
+  const [image, setImage] = useState<File | null>(null);
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const router = useRouter(); // Router instance
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setImage(file);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (formData.password !== formData.passwordConfirmation) {
+      toast.error("Passwords do not match!");
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const result = await authClient.signUp.email({
+        email: formData.email,
+        password: formData.password,
+        name: `${formData.firstName} ${formData.lastName}`,
+        image: image ? await convertImageToBase64(image) : "",
+      });
+
+      if (result.error) {
+        throw new Error(result.error.message);
+      }
+
+      toast.success("Signed up successfully!");
+      router.push("/");
+    } catch (error) {
+      console.error("Sign up failed:", error);
+      toast.error(`${error} || "Sign up failed. Please try again.`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <BackgroundBeamsWithCollision className="flex justify-center min-h-screen w-full bg-gradient-to-br from-gray-100 to-gray-200 p-4">
+      <div className="w-full max-w-md md:max-w-xl flex justify-center items-center">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className="w-full"
+        >
+          <BackgroundGradient className="rounded-[22px] p-5 sm:p-10 bg-white dark:bg-zinc-900 w-full">
+          <Card className="shadow-lg w-full dark:bg-zinc-900">
+            <CardHeader>
+              <CardTitle className="md:text-2xl text-xl font-bold">
+                Sign Up
+              </CardTitle>
+              <CardDescription>
+                Create an account to get started
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form onSubmit={handleSubmit} className="space-y-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                  <div className="space-y-2">
+                    <Label htmlFor="firstName">First name</Label>
+                    <Input
+                      id="firstName"
+                      name="firstName"
+                      placeholder="John"
+                      required
+                      value={formData.firstName}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div className="space-y-2">
+                    <Label htmlFor="lastName">Last name</Label>
+                    <Input
+                      id="lastName"
+                      name="lastName"
+                      placeholder="Doe"
+                      required
+                      value={formData.lastName}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                </div>
+                <div className="space-y-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="john@example.com"
+                    required
+                    value={formData.email}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="space-y-2">
+                  <Label htmlFor="password">Password</Label>
+                  <div className="relative">
+                    <Input
+                      id="password"
+                      name="password"
+                      type={showPassword ? "text" : "password"}
+                      required
+                      value={formData.password}
+                      onChange={handleInputChange}
+                      className="pr-10"
+                    />
+                    <Button
+                      type="button"
+                      variant="ghost"
+                      size="sm"
+                      className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? (
+                        <EyeOffIcon className="h-4 w-4 text-gray-500" />
+                      ) : (
+                        <EyeIcon className="h-4 w-4 text-gray-500" />
+                      )}
+                    </Button>
+                  </div>
+                </div>
+                <div className="space-y-2">
+                  <Label htmlFor="passwordConfirmation">Confirm Password</Label>
+                  <Input
+                    id="passwordConfirmation"
+                    name="passwordConfirmation"
+                    type="password"
+                    required
+                    value={formData.passwordConfirmation}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="space-y-2">
+                  <Label htmlFor="image">Profile Image (optional)</Label>
+                  <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-4">
+                    {imagePreview && (
+                      <div className="relative w-16 h-16 rounded-full overflow-hidden">
+                        <Image
+                          src={imagePreview}
+                          alt="Profile preview"
+                          layout="fill"
+                          objectFit="cover"
+                        />
+                      </div>
+                    )}
+                    <div className="flex-1 flex items-center w-full">
+                      <Input
+                        id="image"
+                        type="file"
+                        accept="image/*"
+                        onChange={handleImageChange}
+                        className="hidden"
+                      />
+                      <Label
+                        htmlFor="image"
+                        className="cursor-pointer flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      >
+                        <Upload className="w-5 h-5 mr-2" />
+                        {imagePreview ? "Change Image" : "Upload Image"}
+                      </Label>
+                      {imagePreview && (
+                        <Button
+                          type="button"
+                          variant="ghost"
+                          size="sm"
+                          onClick={() => {
+                            setImage(null);
+                            setImagePreview(null);
+                          }}
+                        >
+                          <X className="w-4 h-4" />
+                        </Button>
+                      )}
+                    </div>
+                  </div>
+                </div>
+                <Button type="submit" className="w-full dark:bg-zinc-500" disabled={loading}>
+                  {loading ? (
+                    <>
+                      <Loader2 size={16} className="animate-spin mr-2" />
+                      Creating account...
+                    </>
+                  ) : (
+                    "Create account"
+                  )}
+                </Button>
+              </form>
+            </CardContent>
+            <CardFooter>
+              <p className="text-center text-sm text-gray-600 mt-2 w-full">
+                Already have an account?{" "}
+                <Link
+                  href="/sign-in"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  Sign in
+                </Link>
+              </p>
+            </CardFooter>
+            </Card>
+            </BackgroundGradient>
+        </motion.div>
+      </div>
+    </BackgroundBeamsWithCollision>
+  );
+}
+
+async function convertImageToBase64(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result as string);
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
+}
